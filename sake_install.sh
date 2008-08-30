0c9eb2e5293c3f9ad50788826194428c9805e49e
Ben Burkert <ben@benburkert.com>
2008-08-29 19:58:15 -0500

    Added sake install task

---
+++ sake_install.sh
@@ -0,0 +1,7 @@
+sudo gem install gems/rubyforge-1.0.0.gem
+sudo gem install gems/rake-0.8.1.gem
+sudo gem install gems/hoe-1.7.0.gem
+sudo gem install gems/RubyInline-3.7.0.gem
+sudo gem install gems/ParseTree-2.1.1.gem
+sudo gem install gems/ruby2ruby-1.1.8.gem
+sudo gem install gems/sake-1.0.15.gem
