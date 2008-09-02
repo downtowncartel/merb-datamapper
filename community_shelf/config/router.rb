83b16bd07052b4e3d5399e64246a6997945a42e5
Ben Burkert <ben@benburkert.com>
2008-09-02 12:01:38 -0500

    default routing and no population task

--- community_shelf/config/router.rb
+++ community_shelf/config/router.rb
@@ -1,8 +1,4 @@
 Merb.logger.info("Compiling routes...")
 Merb::Router.prepare do |r|
-  r.to(:controller => 'books') do |books|
-    books.match('/book/:slug').to(:action => 'show').name(:book)
-    books.match('/books/:term').to(:action => 'index').name(:books)
-    books.match('/books').to(:action => 'index').name(:books)
-  end
+  r.default_routes
 end
